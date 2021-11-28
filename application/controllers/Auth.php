3e68393662ff31f1f54f34142a598e040d6e0d96
tomiprasetyo <tomi.prasetyo95@gmail.com>
2021-11-28 18:01:17 +0700

    konfigurasi

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -6,10 +6,52 @@ class Auth extends CI_Controller
     function __construct()
     {
         parent::__construct();
+
+        $this->load->model("model_login");
     }
 
     public function index()
     {
         $this->load->view('view_login');
+    }
+
+    public function login_aksi()
+    {
+        $user = $this->input->post("username", true);
+        $pass = md5($this->input->post("password", true));
+
+        // validasi rule
+        $this->form_validation->set_rules("username", "Username", "required");
+        $this->form_validation->set_rules("password", "Password", "required");
+
+        if ($this->form_validation->run() != FALSE) {
+            $where = array(
+                "username" => $user,
+                "password" => $pass,
+            );
+
+            $cek_login = $this->model_login->cek_login($where)->num_rows();
+
+            if ($cek_login > 0) {
+                $session_data = array(
+                    "login" => "OK",
+                    "username" => $user
+                );
+
+                $this->session->set_userdata($session_data);
+
+                redirect(base_url());
+            } else {
+                redirect("auth");
+            }
+        } else {
+            $this->load->view("view_login");
+        }
+    }
+
+    public function logout()
+    {
+        $this->session->sess_destroy();
+        redirect("auth");
     }
 }
--- application/views/view_login.php
+++ application/views/view_login.php
@@ -10,11 +10,11 @@
     <title>Login Klinik Sehat Sejahtera</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="/docs/4.4/dist/css/bootstrap.min.css" rel="stylesheet"
+    <link href="<?= base_url(); ?>assets/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
     <!-- Favicons -->
-    <link rel="icon" href="/docs/4.4/assets/img/favicons/favicon.ico">
+    <link rel="icon" href="<?= base_url(); ?>assets/favicon.ico">
 
 
     <style>
@@ -34,24 +34,23 @@
     }
     </style>
     <!-- Custom styles for this template -->
-    <link href="signin.css" rel="stylesheet">
+    <link href="<?= base_url(); ?>assets/custom/login.css" rel="stylesheet">
 </head>
 
 <body class="text-center">
-    <form class="form-signin">
-        <img class="mb-4" src="/docs/4.4/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
-        <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
-        <label for="inputEmail" class="sr-only">Email address</label>
-        <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
-        <label for="inputPassword" class="sr-only">Password</label>
-        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
-        <div class="checkbox mb-3">
-            <label>
-                <input type="checkbox" value="remember-me"> Remember me
-            </label>
+    <form class="form-signin" method="POST" action="<?= base_url("auth/login_aksi"); ?>">
+        <img class="mb-4" src="<?= base_url(); ?>assets/icon-bootstrap.svg" alt="" width="72" height="72">
+        <h1 class="h3 mb-3 font-weight-normal">Silahkan Login</h1>
+        <div class="alert-danger">
+            <?= validation_errors(); ?>
         </div>
-        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-        <p class="mt-5 mb-3 text-muted">&copy; 2017-2019</p>
+        <label for="Username" class="sr-only">Email address</label>
+        <input type="text" id="username" name="username" class="form-control" placeholder="Username" autofocus>
+        <label for="Password" class="sr-only">Password</label>
+        <input type="password" id="password" name="password" class="form-control" placeholder="Password">
+
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Login</button>
+        <p class="mt-5 mb-3 text-muted">&copy; 2021 - Kelompok 1</p>
     </form>
 </body>
 
