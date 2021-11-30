822fe7134e030579ed4786d4ca16a568944a390a
rizkiakbar2302@gmail.com <rizkiakbar2302@gmail.com>
2021-11-30 23:16:38 +0700

    add model , cont , view rekam medis

--- application/views/kunjungan/view_rekam_medis.php
+++ application/views/kunjungan/view_rekam_medis.php
@@ -11,19 +11,19 @@
                             <tr>
                                 <th>Nama Pasien</th>
                                 <td>:</td>
-                                <td><?= $d['nama_pasien'];?></td>
+                                <td><?= $d['nama_pasien']; ?></td>
                             </tr>
                             <tr>
                                 <th>jenis Kelamin</th>
                                 <td>:</td>
-                                <td><?= $d['jenis_kelamin'];?></td>
+                                <td><?= $d['jenis_kelamin']; ?></td>
                             </tr>
                             <tr>
                                 <th>Umur</th>
                                 <td>:</td>
-                                <td><?= $d['umur'];?></td>
+                                <td><?= $d['umur']; ?></td>
                             </tr>
-                            
+
                         </table>
                     </div>
                 </div>
@@ -43,6 +43,19 @@
                                     <th>Penatalaksanaan</th>
                                 </tr>
                             </thead>
+                            <tbody>
+                                <?php $no = 1;
+                                foreach ($riwayat as $r) { ?>
+                                    <tr>
+                                        <td><?= $no; ?></td>
+                                        <td><?= $r['tgl_berobat']; ?></td>
+                                        <td><?= $r['keluhan_pasien']; ?></td>
+                                        <td><?= $r['hasil_diagnosa']; ?></td>
+                                        <td><?= $r['penatalaksanaan']; ?></td>
+                                    </tr>
+                                <?php $no++;
+                                } ?>
+                            </tbody>
                         </table>
                     </div>
                 </div>
@@ -54,7 +67,28 @@
                         Catatan ( Rekam Medis )
                     </div>
                     <div class="card-body">
-                        table/data rekam medis
+                        <form method="post" action="<?= base_url(); ?>kunjungan/insert_rekam">
+                            <input type="hidden" name="id" value="<?= $d['id_berobat']; ?>">
+                            <div class="form-group">
+                                <label for="">Keluhan</label>
+                                <textarea name="keluhan" class="form-control" required></textarea>
+                            </div>
+                            <div class="form-group">
+                                <label for="">Diagnosa</label>
+                                <textarea name="diagnosa" class="form-control" required></textarea>
+                            </div>
+                            <div class="form-group">
+                                <label for="">Penatalaksanaan</label>
+                                <select name="penatalaksanaan" id="" class="form-control" required>
+                                    <option value="">-Pilih-</option>
+                                    <option value="Rawat Jalan">Rawat Jalan</option>
+                                    <option value="Rawat Inap">Rawat Inap</option>
+                                    <option value="Rujuk">Rujuk</option>
+                                    <option value="Lainnya">Lainnya</option>
+                                </select>
+                            </div>
+                            <button type="submit" class="btn btn-danger btn-sm">Simpan Data</button>
+                        </form>
                     </div>
                 </div>
 
@@ -63,7 +97,16 @@
                         Resep Obat
                     </div>
                     <div class="card-body">
-                        table/data resep obat
+                        <form method="post" action="<?= base_url('kunjungan/insert_resep');?>">
+                                <div class="form-group">
+                                    <label for="obat">Obat</label>
+                                    <select name="obat" id="" class="form-control">
+                                        <?php foreach($obat as $r) {?>
+                                            <option value="<?= $r['id_obat'];?>"><?= $r['nama_obat'];?></option>
+                                        <?php }?>
+                                    </select>
+                                </div>
+                        </form>
                     </div>
                 </div>
             </div>
