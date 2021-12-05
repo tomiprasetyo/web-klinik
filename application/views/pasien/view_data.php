3bf51904d969372490601e5d8ced35965678dd18
rizkiakbar2302@gmail.com <rizkiakbar2302@gmail.com>
2021-12-05 17:30:29 +0700

    edit dash pasien

--- application/views/pasien/view_data.php
+++ application/views/pasien/view_data.php
@@ -1,14 +1,14 @@
 <section class="content mt-2">
     <div class="container-fluid">
         <div class="card border-0">
-            <div class="card-header" style="background-color: #C8E3D4; ">
+            <div class="card-header" style="background-color: #C8E3D4; border-radius:10px;">
                 <?= $title; ?>
 
-                <a href="<?= base_url("pasien/tambah"); ?>" class="btn btn-success btn-sm float-right">Tambah Data</a>
+                <a href="<?= base_url("pasien/tambah"); ?>" class="btn btn-success btn-sm float-right" style="border-radius:10px;">Tambah Data</a>
             </div>
-            <div class="card-body">
+            <div class="card-body animate__animated animate__fadeInDown">
                 <div class="table-responsive">
-                    <table class="table table-bordered table-striped">
+                    <table class="table table-bordered table-hover">
                         <thead>
                             <tr>
                                 <th>No.</th>
@@ -30,8 +30,8 @@
                                     <td><?= $u["jenis_kelamin"]; ?></td>
                                     <td><?= $u["umur"]; ?></td>
                                     <td>
-                                        <a href="<?= base_url() . "pasien/edit/" . $u["id_pasien"]; ?>" class="btn btn-warning btn-sm">Edit</a>
-                                        <a href="<?= base_url() . "pasien/hapus/" . $u["id_pasien"]; ?>" class="btn btn-danger btn-sm" onclick="return confirm('Yakin akan menghapus data?')">Hapus</a>
+                                        <a href="<?= base_url() . "pasien/edit/" . $u["id_pasien"]; ?>" class="btn btn-warning btn-sm" style="border-radius:10px;">Edit</a>
+                                        <a href="<?= base_url() . "pasien/hapus/" . $u["id_pasien"]; ?>" class="btn btn-danger btn-sm" onclick="return confirm('Yakin akan menghapus data?')" style="border-radius:10px;">Hapus</a>
                                     </td>
                                 </tr>
                             <?php $no++;
