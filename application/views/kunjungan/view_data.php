f28dc921894cf56632ac958b8a00bf6231308877
rizkiakbar2302@gmail.com <rizkiakbar2302@gmail.com>
2021-12-05 17:59:26 +0700

    edit dash kunjungan

--- application/views/kunjungan/view_data.php
+++ application/views/kunjungan/view_data.php
@@ -1,50 +1,53 @@
 <section class="content mt-2">
     <div class="container-fluid">
-        <div class="card border-primary">
-            <div class="card-header bg-primary text-white">
-                <?= $title; ?>
+        <div class="container">
 
-                <a href="<?= base_url("kunjungan/tambah"); ?>" class="btn btn-success btn-sm float-right">Kunjungan
-                    Baru</a>
-            </div>
-            <div class="card-body">
-                <div class="table-responsive">
-                    <table class="table table-bordered table-striped">
-                        <thead>
-                            <tr>
-                                <th>No.</th>
-                                <th>Tanggal</th>
-                                <th>Nama Pasien</th>
-                                <th>Umur</th>
-                                <th>Dokter</th>
-                                <th>Rekam Medis</th>
-                                <th>Aksi</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php
+            <div class="card border-0 animate__animated animate__bounceInDown">
+                <div class="card-header bg-info text-white" style="border-radius:10px;">
+                    <?= $title; ?>
 
-                            $no = 1;
-                            foreach ($kunjungan as $u) { ?>
-
+                    <a href="<?= base_url("kunjungan/tambah"); ?>" class="btn btn-success btn-sm float-right" style="border-radius:10px;">Kunjungan
+                        Baru</a>
+                </div>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table table-bordered table-hover">
+                            <thead>
                                 <tr>
-                                    <td class="text-center"><?= $no; ?></td>
-                                    <td><?= $u["tgl_berobat"]; ?></td>
-                                    <td><?= $u["nama_pasien"]; ?></td>
-                                    <td><?= $u["umur"]; ?></td>
-                                    <td><?= $u["nama_dokter"]; ?></td>
-                                    <td>
-                                        <a href="<?= base_url(); ?>kunjungan/rekam/<?= $u["id_berobat"]; ?>" class="btn btn-success btn-sm">Rekam</a>
-                                    </td>
-                                    <td>
-                                        <a href="<?= base_url() . "kunjungan/edit/" . $u["id_berobat"]; ?>" class="btn btn-warning btn-sm">Edit</a>
-                                        <a href="<?= base_url() . "kunjungan/hapus/" . $u["id_berobat"]; ?>" class="btn btn-danger btn-sm" onclick="return confirm('Yakin akan menghapus data?')">Hapus</a>
-                                    </td>
+                                    <th>No.</th>
+                                    <th>Tanggal</th>
+                                    <th>Nama Pasien</th>
+                                    <th>Umur</th>
+                                    <th>Dokter</th>
+                                    <th>Rekam Medis</th>
+                                    <th>Aksi</th>
                                 </tr>
-                            <?php $no++;
-                            } ?>
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody>
+                                <?php
+
+                                $no = 1;
+                                foreach ($kunjungan as $u) { ?>
+
+                                    <tr>
+                                        <td class="text-center"><?= $no; ?></td>
+                                        <td><?= $u["tgl_berobat"]; ?></td>
+                                        <td><?= $u["nama_pasien"]; ?></td>
+                                        <td><?= $u["umur"]; ?></td>
+                                        <td><?= $u["nama_dokter"]; ?></td>
+                                        <td>
+                                            <a href="<?= base_url(); ?>kunjungan/rekam/<?= $u["id_berobat"]; ?>" class="btn btn-success btn-sm" style="border-radius:10px;">Rekam</a>
+                                        </td>
+                                        <td>
+                                            <a href="<?= base_url() . "kunjungan/edit/" . $u["id_berobat"]; ?>" class="btn btn-warning btn-sm" style="border-radius:10px;">Edit</a>
+                                            <a href="<?= base_url() . "kunjungan/hapus/" . $u["id_berobat"]; ?>" class="btn btn-danger btn-sm" onclick="return confirm('Yakin akan menghapus data?')" style="border-radius:10px;">Hapus</a>
+                                        </td>
+                                    </tr>
+                                <?php $no++;
+                                } ?>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
--- application/views/kunjungan/view_edit_data.php
+++ application/views/kunjungan/view_edit_data.php
@@ -1,22 +1,21 @@
 <section class="content mt-2">
-    <div class="container-fluid">
-        <div class="card border-primary">
-            <div class="card-header bg-primary text-white">
+    <div class="container">
+        <div class="card border-0">
+            <div class="card-header bg-info text-white" style="border-radius:10px;">
                 <?= $title; ?>
 
-                <a href="<?= base_url("kunjungan"); ?>" class="btn btn-warning btn-sm float-right">Kembali</a>
+                <a href="<?= base_url("kunjungan"); ?>" class="btn btn-warning btn-sm float-right" style="border-radius:10px;">Kembali</a>
             </div>
-            <div class="card-body">
+            <div class="card-body animate__animated animate__bounceInDown">
                 <form method="POST" action="<?= base_url("kunjungan/update"); ?>">
                     <input type="hidden" name="id" value="<?= $kunjungan['id_berobat']; ?>">
                     <div class="form-group">
                         <label for="">Tanggal Berobat</label>
-                        <input type="date" name="tgl_berobat" value="<?= $kunjungan['tgl_berobat']; ?>"
-                            class="form-control" required>
+                        <input type="date" name="tgl_berobat" value="<?= $kunjungan['tgl_berobat']; ?>" class="form-control" required style="border-radius:10px;">
                     </div>
                     <div class="form-group">
                         <label for="">Pasien</label>
-                        <select name="pasien" id="" class="form-control" required>
+                        <select name="pasien" id="" class="form-control" required style="border-radius:10px;">
                             <?php
                             foreach ($pasien as $u) {
 
@@ -27,7 +26,7 @@
                                 }
 
                             ?>
-                            <option value="<?= $u["id_pasien"]; ?>" <?= $aktif; ?>><?= $u["nama_pasien"]; ?></option>
+                                <option value="<?= $u["id_pasien"]; ?>" <?= $aktif; ?>><?= $u["nama_pasien"]; ?></option>
                             <?php } ?>
                         </select>
                     </div>
@@ -45,12 +44,12 @@
                                 }
 
                             ?>
-                            <option value="<?= $u["id_dokter"]; ?>" <?= $aktif; ?>><?= $u["nama_dokter"]; ?></option>
+                                <option value="<?= $u["id_dokter"]; ?>" <?= $aktif; ?>><?= $u["nama_dokter"]; ?></option>
                             <?php } ?>
                         </select>
                     </div>
                     <div class="form-group">
-                        <button type="submit" class="btn btn-primary btn-sm">Update Data</button>
+                        <button type="submit" class="btn btn-success btn-sm" style="border-radius:10px;">Update Data</button>
                     </div>
                 </form>
             </div>
