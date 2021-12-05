b2193f6212afa2216d3b3a19bbbff46ea2264d90
rizkiakbar2302@gmail.com <rizkiakbar2302@gmail.com>
2021-12-05 17:16:46 +0700

    edit dash user

--- application/views/user/view_edit_data.php
+++ application/views/user/view_edit_data.php
@@ -1,31 +1,31 @@
 <section class="content mt-2">
     <div class="container-fluid">
-        <div class="card border-primary">
-            <div class="card-header bg-primary text-white">
+        <div class="card border-0">
+            <div class="card-header" style="border-radius: 10px; background-color: #F6D7A7;">
                 <?= $title; ?>
 
-                <a href="<?= base_url("users"); ?>" class="btn btn-warning btn-sm float-right">Kembali</a>
+                <a href="<?= base_url("users"); ?>" class="btn btn-warning btn-sm float-right" style="border-radius: 10px;">Kembali</a>
             </div>
-            <div class="card-body">
+            <div class="card-body animate__animated animate__fadeInDown">
                 <form method="POST" action="<?= base_url("users/update"); ?>">
                     <input type="hidden" name="id" value="<?= $users["id"]; ?>">
                     <div class="form-group">
                         <label for="">Username</label>
-                        <input type="text" name="username" value="<?= $users["username"]; ?>" class="form-control"
+                        <input type="text" name="username" value="<?= $users["username"]; ?>" class="form-control" style="border-radius: 10px;"
                             required>
                     </div>
                     <div class="form-group">
                         <label for="">Nama Lengkap</label>
                         <input type="text" name="nama_lengkap" value="<?= $users["nama_lengkap"]; ?>"
-                            class="form-control" required>
+                            class="form-control" style="border-radius: 10px;"required>
                     </div>
                     <div class="form-group">
                         <label for="">Password</label>
                         <input type="password" name="password" class=" form-control"
-                            placeholder="Kosongkan jika tidak merubah password">
+                            placeholder="Kosongkan jika tidak merubah password" style="border-radius: 10px;">
                     </div>
                     <div class="form-group">
-                        <button type="submit" class="btn btn-primary btn-sm">Update Data</button>
+                        <button type="submit" class="btn btn-sm btn-success" style="border-radius: 10px;">Update Data</button>
                     </div>
                 </form>
             </div>
--- application/views/user/view_tambah_data.php
+++ application/views/user/view_tambah_data.php
@@ -1,27 +1,27 @@
 <section class="content mt-2">
     <div class="container-fluid">
-        <div class="card border-primary">
-            <div class="card-header bg-primary text-white">
+        <div class="card border-0">
+            <div class="card-header" style="border-radius: 10px; background-color: #F6D7A7;">
                 <?= $title; ?>
 
-                <a href="<?= base_url("users"); ?>" class="btn btn-warning btn-sm float-right">Kembali</a>
+                <a href="<?= base_url("users"); ?>" class="btn btn-warning btn-sm float-right" style="border-radius: 10px;">Kembali</a>
             </div>
-            <div class="card-body">
+            <div class="card-body animate__animated animate__fadeInDown">
                 <form method="POST" action="<?= base_url("users/insert"); ?>">
                     <div class="form-group">
                         <label for="">Username</label>
-                        <input type="text" name="username" class="form-control" required>
+                        <input type="text" name="username" class="form-control" required style="border-radius: 10px;">
                     </div>
                     <div class="form-group">
                         <label for="">Nama Lengkap</label>
-                        <input type="text" name="nama_lengkap" class="form-control" required>
+                        <input type="text" name="nama_lengkap" class="form-control" required style="border-radius: 10px;">
                     </div>
                     <div class="form-group">
                         <label for="">Password</label>
-                        <input type="password" name="password" class="form-control" required>
+                        <input type="password" name="password" class="form-control" required style="border-radius: 10px;">
                     </div>
                     <div class="form-group">
-                        <button type="submit" class="btn btn-primary btn-sm">Simpan Data</button>
+                        <button type="submit" class="btn btn-success btn-sm" style="border-radius: 10px;">Simpan Data</button>
                     </div>
                 </form>
             </div>
